c5f9c7a867e1017f384decf0643755b5a8248212
shre4ash <shreyashkumbhar9@outlook.com>
2025-05-14 09:35:27 +0530

    tranding section for ..

---
+++ src/components/tranding/Tranding.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './trending.css'
+import TrendingCards from '../trendingcards/TrendingCard'
+
+const Tranding = () => {
+    return (
+        <div>
+               Tranding
+               <TrendingCards/>
+        </div>
+    )
+}
+
+
+export default Tranding
