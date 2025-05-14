b5d379bf8fa9b0f08ccee88b5e4003d9dff4b9d3
shre4ash <shreyashkumbhar9@outlook.com>
2025-05-14 09:07:28 +0530

    componat artist card

---
+++ src/components/artistcard/ArtistCard.jsx
@@ -0,0 +1,11 @@
+import React  from 'react'
+import './artistcard.css'
+const ArtistCard = () => {
+    return(
+        <div>
+              ArtistCard
+        </div>
+    )
+}
+
+export default ArtistCard
