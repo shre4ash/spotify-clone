cb2f2d7196b8fb41571150a5505626c745fec65d
shre4ash <shreyashkumbhar9@outlook.com>
2025-05-14 09:12:14 +0530

    main section

---
+++ src/components/mainsection/mainsection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './mainSection.css'
+import Tranding from '../tranding/Tranding'
+import PopularArtists from '../popularArtists/PopularArtists'
+
+const MainSection = () => {
+    return (
+        <div>
+            <Tranding/>
+            <PopularArtists/>
+        </div>
+    )
+}
+
+
+export default MainSection
