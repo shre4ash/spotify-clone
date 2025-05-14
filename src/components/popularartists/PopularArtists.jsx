50cfc9f7cae9b88187d56470591908454cf95250
shre4ash <shreyashkumbhar9@outlook.com>
2025-05-14 09:22:56 +0530

    popular Artist

---
+++ src/components/popularartists/PopularArtists.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './popularArtists.css'
+import ArtistCard from '../artistcard/ArtistCard'
+const PopularArtists = () =>  {
+    return (
+        <div>
+            PopularArtists
+            <ArtistCard/>
+        </div>
+    )
+}
+
+export default PopularArtists
